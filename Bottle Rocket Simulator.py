aa349157cf6230fda34ba96c8b30f6d8c5a1b3db
Jesse Wood <jhwblender@gmail.com>
2019-05-10 11:43:32 -0600

    Added Specific Impulse
    Added Specific Impulse, Also added comments for units

--- Bottle Rocket Simulator.py
+++ Bottle Rocket Simulator.py
@@ -1,19 +1,20 @@
 from math import *
 watDensity = 1000 #kg/m^3
 airDensity = 1.275 #kg/m^3
-gravity = 9.8 #m/s^2
+gravityInitial = 9.8 #m/s^2
+avgRadiusEarth = 6371.0 #km
 accuracy = 3 # number of digits
 
-timeStep = float(input("Time step: "))
-pressure = float(input("pressure (psi): "))
-volume = float(input("Total volume (liters): "))
-iMix = float(input("Percent water: "))
-emptyMass = float(input("Empty weight (kg): "))
-rocketRadius = float(input("Rocket Radius (cm): "))
-nozzleRadius = float(input("Nozzle radius (cm): "))
+timeStep = float(input("Time step: ")) #seconds
+pressure = float(input("pressure (psi): ")) #psi (later converted to Pa)
+volume = float(input("Total volume (liters): ")) #liters
+iMix = float(input("Percent water: ")) #percent
+emptyMass = float(input("Empty weight (kg): ")) #kg
+rocketRadius = float(input("Rocket Radius (cm): ")) #cm (later converted to m)
+nozzleRadius = float(input("Nozzle radius (cm): ")) #cm (later converted to m)
 
 iMix = iMix/100 #Changing to percent decimal
-pressure = pressure*6895 #Converting to Pa
+pressure = pressure*6894.7572931783 #Converting to Pa (N/m^2)
 pressureInitial = pressure
 volume = volume*1e-3 #Converting to M^3
 nozzleRadius = nozzleRadius*1e-2 #Converting to M
@@ -24,24 +25,30 @@ airvolumeInitial = airvolume
 watVolume = volume*iMix
 watVolumeInitial = watVolume
 
-time = 0
-acc = 0
-maxAcc = 0
-vel = 0
-pos = 0
-watVel = 0
-volumeFlowRate = 0
-massFlowRate = 0
-thrustForce = 0
-airResistance = 0
-weight = 0
-mass = 0
+time = 0 #seconds
+acc = 0 #m/s^2
+maxAcc = 0 #m/s^2
+vel = 0 #m/s
+pos = 0.0 #m
+watVel = 0 #m/s
+volumeFlowRate = 0 #m^3/s
+massFlowRate = 0 #kg/s
+thrustForce = 0 #N
+isp = 0 #m/s
+maxIsp = 0
+avgIsp = 0
+airResistance = 0 #N (kg m/s^2)
+mass = 0 #kg
+gravity = gravityInitial
 latentLiftoff = False
 latentRecorded = False
 latentLiftoffTime = 0
 latentLiftoffWatPercentage = 0
-while 1:
-    print("Time: "+str(round(time,accuracy))+"s - vel: "+str(round(vel,accuracy))+"m/s - Height: "+str(round(pos*3.281,accuracy))+"ft - Water: "+str(round((watVolume/watVolumeInitial)*100,accuracy))+"%")
+
+loopCount = 0
+while 1: #This loop is specifically run only when the rocket is firing
+    gravity = (gravityInitial*pow(avgRadiusEarth,2))/float(pow((pos/1000.0)+avgRadiusEarth,2))
+    print("Time: "+str(round(time,accuracy))+"s - vel: "+str(round(vel,accuracy))+"m/s - Height: "+str(round(pos*3.281,accuracy))+"ft - Water: "+str(round((watVolume/watVolumeInitial)*100,accuracy))+"%"+" - isp: "+str(round(isp,accuracy)))
     time = time+timeStep
     watVel = sqrt((2*pressure)/watDensity)
     volumeFlowRate = pi*pow(nozzleRadius,2)*watVel
@@ -52,7 +59,11 @@ while 1:
     airvolume = airvolume+(volumeFlowRate*timeStep)
     pressure = (pressureInitial*airvolumeInitial)/airvolume
     thrustForce = pi*pow(nozzleRadius,2)*pressure
-    airResistance = .5*airDensity*pow(vel,2)*pi*pow(rocketRadius,2)
+    isp = thrustForce/massFlowRate
+    if(isp>maxIsp):
+        maxIsp = isp
+    avgIsp += isp
+    airResistance = .5*airDensity*pow(vel,2)*pi*pow(rocketRadius,2) #Calculating Air Resistance
     mass = emptyMass+(watVolume*watDensity)
     acc = (thrustForce-(mass*gravity)-airResistance)/mass
     if(acc>maxAcc):
@@ -67,12 +78,14 @@ while 1:
             latentLiftoffWatPercentage = (watVolume/watVolumeInitial)*100
             latentRecorded = True
     pos = pos+(vel*timeStep)
+    loopCount += 1
 
 burnTime = time
 maxVel = vel
+avgIsp /= loopCount
 
 while(vel>0):
-    print("Time: "+str(round(time,accuracy))+"s - vel: "+str(round(vel,accuracy))+"m/s - height: "+str(round(pos*3.281,accuracy))+"ft")
+    print("Time: "+str(round(time,accuracy))+"s - vel: "+str(round(vel,accuracy))+"m/s - height: "+str(round(pos*3.281,accuracy))+"ft"+" - gravity: "+str(round(gravity,accuracy)))
     time= time+timeStep
     vel = vel-(gravity*timeStep)
     pos = pos+(vel*timeStep)
@@ -83,4 +96,6 @@ if(latentLiftoff and not latentRecorded):
 if(latentLiftoff):
     print("Time till liftoff: "+str(round(burnTime,accuracy))+" seconds\nLiftoff Water Percentage: "+str(round(latentLiftoffWatPercentage,accuracy))+"%")
 print("Rocket Height: "+str(round(volume/(pi*pow(rocketRadius,2)),accuracy))+" m")
-input("Burn time: "+str(round(burnTime,accuracy))+" seconds\nMax G-Force: "+str(round(maxAcc/9.8,accuracy))+" g's\nMax Velocity: "+str(round(maxVel,accuracy))+" m/s ("+str(round(maxVel*2.237,accuracy))+" mph)"+"\nTime to peak: "+str(round(time,accuracy))+" seconds\nPeak Height: "+str(round(pos*3.281,accuracy))+" ft")
+print("Max isp: " + str(round(maxIsp,accuracy)) + " m/s or " + str(round(maxIsp/9.80665,accuracy)) + " s")
+print("Avg isp: " + str(round(avgIsp,accuracy)) + " m/s or " + str(round(avgIsp/9.80665,accuracy)) + " s")
+input("Burn time: "+str(round(burnTime,accuracy))+" seconds\nMax G-Force: "+str(round(maxAcc/9.8,accuracy))+" g's\nMax Velocity: "+str(round(maxVel,accuracy))+" m/s ("+str(round(maxVel*2.237,accuracy))+" mph)"+"\nTime to peak: "+str(round(time,accuracy))+" seconds\nPeak Height: "+str(round(pos*3.281,accuracy))+" ft ("+str(round(pos,accuracy))+" m)")
